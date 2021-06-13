74579605f2ffff6929846672e4eb9ebb1dbb0014
nurulam98 <nurulakbarmalik98@gmail.com>
2021-06-13 17:46:15 +0700

    Update Privacy Policy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('vendor/font-awesome/css/fontawesome-all.css') }}">
     <!-- Custom Css -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
@@ -19,16 +19,17 @@
     <!-- Navbar -->
     @include('includes.navbarHome')
     <!-- Akhir Navbar -->
-      {{-- Jumbotron --}} 
+      {{-- Jumbotron --}}
      <div class="jumbotron">
           <div class="container">
           </div>
-      </div> 
+      </div>
     {{-- Akhir Jumbotron --}}
     <script src="{{ asset('js/app.js') }}"></script>
 <footer class="footer" id="sticky-footer">
 		<div class="container text-center">
 			<span class="text-muted" style="vertical-align: middle; line-height: 60px; margin-right:auto;"><a href="https://www.facebook.com/confidenceadultcare/" class="fa fa-facebook"></a> <a href="https://www.instagram.com/confidenceadultcare/" class="fa fa-instagram" style="margin-left:1%;"></a></span>
+
 		</div>
 	</footer>
   </body>
